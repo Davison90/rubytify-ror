28368a0fb13b57f34beaee6acaa00dabf717e751
Angela Zaldua <angelazaldua29@gmail.com>
2021-11-08 23:40:47 -0500

    Update information

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -3,8 +3,10 @@ require 'json'
 module Api
   module V1
     class ArtistsController < ApplicationController
-
+      
       def index
+
+        host = request.base_url
         RSpotify.authenticate("37a318ec153f4cfda51cc89bf51f9da1", "f9d072897164452f96cf592f1d73c5ee")
         @artists = Artist.all.select('id, name, image, genres,  popularity, spotify_url, spotify_id').order('popularity DESC')
 
@@ -20,7 +22,7 @@ module Api
 
           info_artist += "<strong>popularity:</strong> #{art.popularity}<br>"
           info_artist += "<strong>spotify_url:</strong> <a href=#{art.spotify_url}>#{art.spotify_url}</a> <br>"
-          info_artist += "<a href=https://desolate-reaches-10571.herokuapp.com/api/v1/artists/#{art.spotify_id}/albums>Ver albumes</a><br>"
+          info_artist += "<a href=#{host}/api/v1/artists/#{art.spotify_id}/albums>Ver albumes</a><br>"
           info_artist += "-----------------------------------------------------------------<br>"
         end
 
@@ -28,6 +30,7 @@ module Api
       end
 
       def show_albums
+        host = request.base_url
         @albums = Album.where(spotify_id_artist: params[:spotify_id_artist])
         info_album = ""
 
@@ -49,7 +52,7 @@ module Api
             info_album += "<strong>total_tracks:</strong> SIN DATOS<br>"
           end
 
-          info_album += "<strong>Songs of the album:</strong> <a href=https://desolate-reaches-10571.herokuapp.com/api/v1/albums/#{alb.spotify_id}/songs>#{alb.spotify_id}</a><br>"
+          info_album += "<strong>Songs of the album:</strong> <a href=#{host}/api/v1/albums/#{alb.spotify_id}/songs>#{alb.spotify_id}</a><br>"
           info_album += "--------------------------------------------------------------------<br>"
         end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
   def index
+    puts request.base_url
+    host = request.base_url
     info_song = ""
-    info_song += "<strong>Ver lista de artistas:</strong> <a href=https://desolate-reaches-10571.herokuapp.com/api/v1/artists>Artistas</a><br>"
-    info_song += "<strong>Buscar canciones aleatorias por genero:</strong> <a href=https://desolate-reaches-10571.herokuapp.com/api/v1/genres/salsa/random_song>Default: Salsa</a><br>"
+    info_song += "<strong>Ver lista de artistas:</strong> <a href=#{host}/api/v1/artists>Artistas</a><br>"
+    info_song += "<strong>Buscar canciones aleatorias por genero:</strong> <a href=#{host}/api/v1/genres/salsa/random_song>Default: Salsa</a><br>"
     render html: "#{info_song}".html_safe, content_type: 'text/html'
   end
 end

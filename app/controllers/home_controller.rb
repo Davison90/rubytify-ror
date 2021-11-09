7734cf57197c90866f4356d69c737e9579f33a06
Angela Zaldua <angelazaldua29@gmail.com>
2021-11-08 23:14:11 -0500

    Update information

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     info_song = ""
-    info_song += "<strong>Ver lista de artistas:</strong> <a href=http://localhost:3000/api/v1/artists>Artistas</a><br>"
-    info_song += "<strong>Buscar canciones aleatorias por genero:</strong> <a href=http://localhost:3000/api/v1/genres/salsa/random_song>Default: Salsa</a><br>"
+    info_song += "<strong>Ver lista de artistas:</strong> <a href=https://desolate-reaches-10571.herokuapp.com/api/v1/artists>Artistas</a><br>"
+    info_song += "<strong>Buscar canciones aleatorias por genero:</strong> <a href=https://desolate-reaches-10571.herokuapp.com/api/v1/genres/salsa/random_song>Default: Salsa</a><br>"
     render html: "#{info_song}".html_safe, content_type: 'text/html'
   end
 end
